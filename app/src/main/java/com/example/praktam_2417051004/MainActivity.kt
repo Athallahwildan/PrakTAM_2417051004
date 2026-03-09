7ae570c04e4d5bc514e6f8aa69c8e4e7a33134f2
Athallahwildan <athallahwildanrafi23@gmail.com>
2026-03-09 15:07:25 +0700

    Tugas LKP 3

--- app/src/main/java/com/example/praktam_2417051004/MainActivity.kt
+++ app/src/main/java/com/example/praktam_2417051004/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.praktam_2417051004
 
+import com.example.praktam_2417051004.model.Cemil
+import com.example.praktam_2417051004.model.FoodSource
+import com.example.praktam_2417051004.ui.theme.PrakTAM_2417051004Theme
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,108 +10,111 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.praktam_2417051004.model.Cemil
-import com.example.praktam_2417051004.model.FoodSource
-import com.example.praktam_2417051004.ui.theme.PrakTAM_2417051004Theme
 import androidx.compose.foundation.Image
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.*
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.graphics.Color
+import androidx.compose.foundation.border
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             PrakTAM_2417051004Theme{
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column(modifier = Modifier.padding(innerPadding)) {
-                        Greeting()
-                    }
-                }
+                DaftarMakananScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting() {
-    val listCemil = FoodSource.dummyFood
-
+fun DaftarMakananScreen() {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp)
+            .verticalScroll(rememberScrollState())
+            .padding(24.dp)
     ) {
+        FoodSource.dummyFood.forEach { food ->
+            DetailScreen(food = food)
+            Spacer(modifier = Modifier.height(24.dp))
+        }
+    }
+}
 
-        Text(
-            text = "Cemilin",
-            fontSize = 24.sp,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(bottom = 16.dp)
-        )
+@Composable
+fun DetailScreen(food: Cemil) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .border(4.dp, Color.Gray)
+            .padding(16.dp)
+    ){
+        Row {
+            Image(
+                painter = painterResource(id = food.imageRes),
+                contentDescription = food.nama,
+                modifier = Modifier
+                    .size(120.dp),
+                contentScale = ContentScale.Crop
+            )
 
-        LazyColumn(
-            verticalArrangement = Arrangement.spacedBy(12.dp)
-        ) {
-            items(listCemil) {cemil ->
+            Spacer(modifier = Modifier.width(10.dp))
 
-                Card(
-                    modifier = Modifier.fillMaxWidth(),
-                    shape = RoundedCornerShape(12.dp)
-                ) {
+            Column {
+                Text(
+                    text = food.nama,
+                    style = MaterialTheme.typography.headlineMedium,
+                    fontWeight = FontWeight.Bold
+                )
 
-                    Row(
-                        modifier = Modifier.padding(12.dp)
-                    ) {
-                        Image(
-                            painter = painterResource(id = cemil.imageRes),
-                            contentDescription = cemil.nama,
-                            modifier = Modifier
-                                .size(100.dp),
-                            contentScale = ContentScale.Crop
-                        )
+                Spacer(modifier = Modifier.height(8.dp))
 
-                        Spacer(modifier = Modifier.width(12.dp))
+                Text(
+                    text = food.deskripsi,
+                    style = MaterialTheme.typography.bodyLarge
+                )
 
-                        Column {
-                            Text(
-                                text = cemil.nama,
-                                fontWeight = FontWeight.Bold
-                            )
-                            Text(text = cemil.deskripsi)
-                            Text(
-                                text = "Rp ${cemil.harga}",
-                                color = MaterialTheme.colorScheme.primary
-                            )
-                        }
-                    }
-                }
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Text(
+                    text = "Harga: Rp ${food.harga}",
+                    style = MaterialTheme.typography.bodyLarge
+                )
             }
         }
+
+        Spacer(modifier = Modifier.height(6.dp))
+
+        Button(
+            onClick = {},
+                modifier = Modifier.fillMaxWidth(),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFFFF9800)
+                )
+            ){
+                Text("Pesan Sekarang")
+            }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun DaftarMakananPreview(){
     PrakTAM_2417051004Theme{
-        Greeting()
+        DaftarMakananScreen()
     }
 }
